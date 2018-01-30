e9ac1ff38a17d9c7d3872de253028feb4f8f4a83
Shivaprasad Hegde <shivaprasad.hegde@hpe.com>
2018-01-30 12:14:50 +0530

    updated with v1.6

---
+++ gateway/src/main/resources/static/js/display.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	 $.ajax({
+		    type: "GET",
+			url: "http://localhost:7000/products/allproducts",
+			dataType: "json",
+			async: "false",
+			crossDomain: true,
+		    contentType: "application/json; charset=utf-8",
+		    success: function(data) {
+			//alert(data);
+				 var html = '';
+
+		            for ( var i in data ) {
+		            
+		               
+		                    html += "   <tr>";
+		                    html += "       <td  >"+" </td>";
+		                    html += "       <td>"+data[i].productId+"&nbsp;&nbsp; "+"</td>";
+		                    html += "       <td>"+data[i].productName+"&nbsp;&nbsp; "+"</td>";
+		                    html += "       <td>"+data[i].productPrice+"</td>";
+		                    html += "   </tr><br/>";
+		                   
+		                    
+		            	
+		            }
+
+		         $('table').append(html);
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+                      alert(jqXHR+" - "+textStatus+" - "+errorThrown);
+            }
+		});
+});
+
+
+
+
